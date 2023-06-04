b73e9ce8b86346f6abddc920b429740e8f27de1e
SearchEngine <111691329+Boks-Boks@users.noreply.github.com>
2023-06-04 14:20:39 +0200

    Add files via upload

---
+++ src/utilities.js
@@ -0,0 +1,20 @@
+export const drawRect = (detections, ctx) => {
+    detections.forEach (prediction => {
+        //Get prediction results
+        const [x, y, width, height] = prediction ['bbox'];
+        const text = prediction ['class'];
+
+        //set styling
+        const color = "green"
+        ctx.strokeStyle = color
+        ctx.font = '18px Arral'
+        ctx.fillStyle = "red"
+
+        //Draw retangles and text
+        
+        ctx.beginPath()
+        ctx.fillText(text, x, y)
+        ctx.rect(x, y, width, height)
+        ctx.stroke()
+    })
+}
